6f37ab134ffd3f9f7162485607ab397f807473c7
Devin Lee-Jones <devinleejones@gmail.com>
2018-10-02 16:18:33 -0700

    Users input data is saved and logs the input in the console and refreshes form state. #1

--- user-can-create-a-flash-card/src/card.js
+++ user-can-create-a-flash-card/src/card.js
@@ -18,22 +18,54 @@ const styles = {
 export default class Card extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      question: '',
+      answer: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
-
+  handleChange(event) {
+    const form = event.target
+    this.setState({
+      [form.name]: form.value
+    })
+  }
+  handleSubmit(event) {
+    event.preventDefault()
+    console.log(this.state)
+    event.target.reset()
+  }
   render() {
+    const { handleChange } = this
+    const { handleSubmit } = this
+    const { value } = this.state
     return (
       <div className="container-fluid d-flex justify-content-center">
         <span style={styles.border}>
           <h1 style={styles.title}>Create a Flash Card</h1>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="form-group m-4 pt-4">
               <label htmlFor="exampleQuestion">Question</label>
-              <input name="question" type="question" className="form-control" />
+              <input
+                name="question"
+                type="question"
+                value={value}
+                onChange={handleChange}
+                className="form-control"
+                placeholder="Input a Question"
+              />
             </div>
             <div className="form-group m-4">
               <label htmlFor="exampleAnswer">Answer</label>
-              <input name="answer" type="answer" className="form-control" />
+              <input
+                name="answer"
+                type="answer"
+                value={value}
+                onChange={handleChange}
+                className="form-control"
+                placeholder="Input an Answer"
+              />
             </div>
             <button type="save">Save</button>
           </form>
