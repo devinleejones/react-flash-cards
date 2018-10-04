e315814839b08716173091362d9f602ef04d5c71
Devin Lee-Jones <devinleejones@gmail.com>
2018-10-04 13:33:05 -0700

    Edited cards component with an <a> tag with an href styled as a button pointing to the form page once clicked. #3

--- src/cards.js
+++ src/cards.js
@@ -4,16 +4,21 @@ const styles = {
   position: {
     width: '25rem'
   },
-  button: {
+  a: {
     border: '2px solid black',
     height: '2.5rem',
+    padding: '1rem',
     transform: 'translateY(2rem)',
     boxShadow: '2px 2px 2px rgb(0, 0, 0)',
     cursor: 'pointer',
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    textDecoration: 'none',
+    fontSize: '24px',
+    color: 'black'
   },
   text: {
-    marginTop: '10rem'
+    marginTop: '10rem',
+    marginBottom: '2rem'
   }
 }
 
@@ -21,9 +26,9 @@ export default function Cards() {
   return (
     <div className="mx-auto text-center" style={styles.position}>
       <h1 style={styles.text}>You have no flash cards</h1>
-      <button type="submit" style={styles.button}>
+      <a href="#new" style={styles.a}>
         Make One
-      </button>
+      </a>
     </div>
   )
 }
