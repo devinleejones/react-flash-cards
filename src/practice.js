2ea72f0f20be7a1dd241c166527a8478786971fb
Devin Lee-Jones <devinleejones@gmail.com>
2018-10-08 16:20:54 -0700

    Updated practice component to only render one card at a time. Added a next and previous icon to sort through our flashcards. #11

--- src/practice.js
+++ src/practice.js
@@ -21,7 +21,16 @@ const styles = {
   },
   title: {
     fontWeight: 'bold',
-    fontSize: '42px'
+    fontSize: '28px'
+  },
+  previous: {
+    transform: 'translateY(4.5rem) translateX(-2rem)',
+    cursor: 'pointer'
+  },
+  next: {
+    transform: 'translateY(-4rem) translateX(31rem)',
+    position: 'd-inline',
+    cursor: 'pointer'
   }
 }
 
@@ -33,21 +42,61 @@ export default class Practice extends Component {
       showAnswer: false
     }
     this.showAnswer = this.showAnswer.bind(this)
+    this.changeFlashCard = this.changeFlashCard.bind(this)
   }
 
   showAnswer() {
-    this.setState({ showAnswer: !this.state.showAnswer })
+    this.setState({
+      showAnswer: !this.state.showAnswer
+    })
+  }
+
+  changeFlashCard(event) {
+    const targetCard = event.target.id
+    const { currentCard } = this.state
+    const { cards } = this.props
+    if (cards.length - 1 > currentCard) {
+      if (targetCard === 'previous' && currentCard > 0) {
+        this.setState({
+          currentCard: currentCard - 1,
+          showAnswer: false
+        })
+      }
+      else if (targetCard === 'next') {
+        this.setState({
+          currentCard: currentCard + 1,
+          showAnswer: false
+        })
+      }
+    }
+    else if (cards.length - 1 === currentCard) {
+      if (targetCard === 'previous') {
+        this.setState({
+          currentCard: currentCard - 1,
+          showAnswer: false
+        })
+      }
+      else {
+        this.setState({ currentCard: 0, showAnswer: false })
+      }
+    }
   }
 
   render() {
-    const { showAnswer } = this.state
+    const { currentCard, showAnswer } = this.state
     const { cards } = this.props
     const answer = showAnswer ? '' : 'd-none'
     const answerButton = showAnswer ? 'Hide Answer' : 'Show Answer'
     return (
       <div style={styles.container}>
+        <i
+          style={styles.previous}
+          onClick={this.changeFlashCard}
+          id="previous"
+          className="fas fa-less-than fa-lg"
+        />
         <div className="card-body" style={styles.menu}>
-          <h3 style={styles.title}>{cards[0].question}</h3>
+          <h3 style={styles.title}>{cards[currentCard].question}</h3>
           <a>
             <i
               onClick={this.showAnswer}
@@ -57,9 +106,17 @@ export default class Practice extends Component {
             {answerButton}
           </a>
           <p style={styles.p} className={`mt-2 ml-1 ${answer}`}>
-            {cards[0].answer}
+            {cards[currentCard].answer}
           </p>
         </div>
+        <span>
+          <i
+            style={styles.next}
+            onClick={this.changeFlashCard}
+            id="next"
+            className="fas fa-greater-than fa-lg"
+          />
+        </span>
       </div>
     )
   }
